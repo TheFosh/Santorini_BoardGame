44735be949b3d72b2db37f864304217cc6071b16
TheFosh <jakeswan213@gmail.com>
2025-01-13 14:51:54 -0600

    Made method for looking into all moves possible for a player.

--- ArtificialPlayer.py
+++ ArtificialPlayer.py
@@ -22,14 +22,31 @@ class GameEvaluator:
 
         return pieces
 
-    def search_once(self, picked_player):
-
-
-    def turn_generator(self, player_num):
+    def search_moves(self, relevant_player_num):
         """
-        Since a turn consists of three points that are needed
+        :return: All possible turns for the pieces of the relevant player
         """
+        piece_turns = []
+        pieces = self.get_player_pieces(relevant_player_num)
 
+        for p in pieces:
+            piece_turns.append(self.turn_generator(p))
+
+        return piece_turns
+
+    def turn_generator(self, player_piece):
+        """
+        Makes an array of moves and builds around the given player piece.
+        """
+        all_turns = []
+
+        all_moves = self.current_board.get_spaces_around(player_piece)
+        possible_moves = self.current_board.move_filter(all_moves, player_piece)
+        for m in possible_moves:
+            possible_builds = self.current_board.get_spaces_around(m)
+            all_turns.append(m)
+            for b in possible_builds:
+                all_turns.append(b)
 
     def total_score(self, player_piece, potential_spot):
         return (self.winning_score(player_piece, potential_spot)
@@ -77,4 +94,14 @@ class GameEvaluator:
             weights.append(score * distance_weight)
 
         return weights[0] + weights[1]
+
+    def evaluate_next_step(self, relevant_player):
+        """
+        Every call looks 1 step further into possible moves
+        from where it left off.
+        """
+
+        for i in self.Depth:
+            all_turns = self.search_moves(relevant_player)
+
 
--- Runner.py
+++ Runner.py
@@ -17,12 +17,12 @@ GAME_GUI = GUI(SCREEN_HEIGHT, SCREEN_WIDTH, WIDTH_OFFSET, BOARD_CELL_COUNT)
 def main():
     GAME_GUI.setup()
     GAME_GUI.setup_game()
-    GAME_GUI.start_game()
 
     win = GAME_GUI.get_window()
 
     while True:
         mouse = win.checkMouse()
         if mouse != None:
+            GAME_GUI.next_turn(mouse.getX(), mouse.getY())
             mouse.draw(win)
 main()
